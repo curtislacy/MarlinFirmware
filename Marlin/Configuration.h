38f6ce9e5cd407513a75e7cea1d05f1bcc6fab67
Curtis <curtis.lacy@gmail.com>
2019-05-14 17:54:38 -0400

    Tweak XY calibration, adjust esteps, remove SD support, and bring back EEPROM settings and the axis homing menus

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -685,7 +685,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 405 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 81, 81, 400, 465 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -1356,7 +1356,7 @@
 // M501 - reads parameters from EEPROM (if you need reset them after you changed them temporarily).
 // M502 - reverts to the default "factory settings".  You still need to store them in EEPROM afterwards if you want to.
 //
-//#define EEPROM_SETTINGS // Enable for M500 and M501 commands
+#define EEPROM_SETTINGS // Enable for M500 and M501 commands
 //#define DISABLE_M503    // Saves ~2700 bytes of PROGMEM. Disable for release!
 //#define EEPROM_CHITCHAT   // Give feedback on EEPROM commands. Disable to save PROGMEM.
 
@@ -1567,7 +1567,7 @@
  * you must uncomment the following option or it won't work.
  *
  */
-#define SDSUPPORT
+//#define SDSUPPORT
 
 /**
  * SD CARD: SPI SPEED
@@ -1639,7 +1639,7 @@
 //
 // Add individual axis homing items (Home X, Home Y, and Home Z) to the LCD menu.
 //
-//#define INDIVIDUAL_AXIS_HOMING_MENU
+#define INDIVIDUAL_AXIS_HOMING_MENU
 
 //
 // SPEAKER/BUZZER
