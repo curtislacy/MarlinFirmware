bff0af3a9013e6b95c9687c7c585c14662e74a62
Curtis <curtis.lacy@gmail.com>
2019-07-13 11:00:07 -0400

    Reduce ESteps based on 75% flow rate to avoid overextrustion.   Reduced X/Y steps per unit due to slightly too-large big pieces.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -685,7 +685,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 81, 81, 400, 581 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 436 }
 
 /**
  * Default Max Feed Rate (mm/s)
